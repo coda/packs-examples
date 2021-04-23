ce0e9f195e436a281176b98b058e814225fbf490
Jonathan Goldman <jonathan@coda.io>
2021-04-23 20:26:48 +0000

    Fix

--- examples/dictionary/test/dictionary_integration.ts
+++ examples/dictionary/test/dictionary_integration.ts
@@ -9,7 +9,6 @@ describe('Dictionary pack integration test', () => {
     // Here we execute the formula using a real http fetcher. Since this pack requires authentication,
     // this requires that you've already run `coda auth examples/dictionary/manifest.ts` to set up
     // an API key.
-    console.log('resolve', require.resolve('../manifest'));
     const response = await executeFormulaFromPackDef(manifest, 'Define', ['coda'], undefined, undefined, {
       useRealFetcher: true,
       manifestPath: require.resolve('../manifest'),
