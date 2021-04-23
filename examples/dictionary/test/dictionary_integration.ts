e65e2ba5ea10a81de530bd46e6410e98ff6aaf6c
Jonathan Goldman <jonathan@coda.io>
2021-04-23 20:23:20 +0000

    Update examples to use new credentials file layout.

--- examples/dictionary/test/dictionary_integration.ts
+++ examples/dictionary/test/dictionary_integration.ts
@@ -9,8 +9,10 @@ describe('Dictionary pack integration test', () => {
     // Here we execute the formula using a real http fetcher. Since this pack requires authentication,
     // this requires that you've already run `coda auth examples/dictionary/manifest.ts` to set up
     // an API key.
+    console.log('resolve', require.resolve('../manifest'));
     const response = await executeFormulaFromPackDef(manifest, 'Define', ['coda'], undefined, undefined, {
       useRealFetcher: true,
+      manifestPath: require.resolve('../manifest'),
     });
 
     assert.isAtLeast(response.length, 1);
