781d046b9fb7e50f030708cbd24a512043bf2662
Eric Koleda <ekoleda@coda.io>
2023-12-27 17:35:15 -0500

    Add a Vonage (JWT auth) example

---
+++ examples/vonage/credentials.ts
@@ -0,0 +1,16 @@
+// A file to hold the credentials used by the Pack.
+// It's not possible to use the SDK's system authentication and store these in
+// Coda directly since you need direct access to the values in order to create
+// the JWT.
+// We recommend you don't check this file into your code repository, for example
+// by adding an entry for it in your .gitignore file.
+
+// The ID of your Vonage application.
+export const ApplicationId = "...";
+
+// The private key generated for your Vonage application.
+export const PrivateKey = `
+-----BEGIN PRIVATE KEY-----
+...
+-----END PRIVATE KEY-----
+`.trim();
